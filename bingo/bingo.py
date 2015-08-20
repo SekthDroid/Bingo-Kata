caa8f7fc202a8017947d13c7da484f402fce2394
SekthDroid <secthndamon@gmail.com>
2015-08-20 20:51:22 +0200

    Added new feature for Bingo that allows generate numbers between 1 and 75
    Now the Bingo can return all numbers from 1 to 75 inclusive and all of
    them are called only 1 time (No duplicated numbers can be present)

--- bingo/bingo.py
+++ bingo/bingo.py
@@ -1,6 +1,19 @@
+from random import randrange
+
 __author__ = 'SekthDroid'
 
 
 class Bingo(object):
+    available_numbers = []
+
+    def __init__(self):
+        super().__init__()
+        self.available_numbers = [i for i in range(1, 75)]
+
     def call_number(self):
-        return 1
+        if len(self.available_numbers) is 0:
+            return
+        
+        random = self.available_numbers[randrange(0, len(self.available_numbers))]
+        self.available_numbers.remove(random)
+        return random
--- tests/bingo_test.py
+++ tests/bingo_test.py
@@ -5,9 +5,19 @@ __author__ = 'SekthDroid'
 
 
 class BingoTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.bingo = Bingo()
 
     def test_bingo_call_number_should_return_number_between_one_and_seventyfive_inclusive(self):
-        bingo = Bingo()
-        number = bingo.call_number()
-        self.assertGreaterEqual(1, number)
+        number = self.bingo.call_number()
+        self.assertGreaterEqual(number, 1)
         self.assertLessEqual(number, 75)
+
+    def test_bingo_call_number_should_return_unique_random_numbers_between_one_and_seventyfive_inclusive(self):
+        numbers = set()
+
+        for i in range(0, 75):
+            numbers.add(self.bingo.call_number())
+
+        self.assertEqual(75, len(numbers))
