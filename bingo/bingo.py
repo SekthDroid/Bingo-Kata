e1c7ae70980c1bd21950dc568cb9a32a6d690499
SekthDroid <secthndamon@gmail.com>
2015-08-20 20:26:28 +0200

    Added test for call a number that should be between 1 and 75 inclusive

--- bingo/bingo.py
+++ bingo/bingo.py
@@ -2,4 +2,5 @@ __author__ = 'SekthDroid'
 
 
 class Bingo(object):
-    pass
+    def call_number(self):
+        return 1
--- tests/bingo_test.py
+++ tests/bingo_test.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
+from bingo.bingo import Bingo
 
 __author__ = 'SekthDroid'
 
 
 class BingoTest(TestCase):
-    pass
+
+    def test_bingo_call_number_should_return_number_between_one_and_seventyfive_inclusive(self):
+        bingo = Bingo()
+        number = bingo.call_number()
+        self.assertGreaterEqual(1, number)
+        self.assertLessEqual(number, 75)
